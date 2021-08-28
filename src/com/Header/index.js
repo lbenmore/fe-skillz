767e63f3cc9c643cbe250162ce8dd0cef3dcb082
Lisa Benmore <lbenmore@gmail.com>
2021-08-28 15:23:57 -0700

    states and animations working

--- src/com/Header/index.js
+++ src/com/Header/index.js
@@ -1,19 +1,38 @@
+import React from 'react';import Hamburger from '../Hamburger';
 import NavItem from '../NavItem';
 import Logo from '../Logo';
 import pages from '../../pages.js';
 import './index.scss';
 
-export default function Header () {
-  const isMobile = window.innerWidth <= 1024;
-  const width = isMobile ? '100px' : '120px';
-  const height = '100%';
+class Header extends React.Component {
+  constructor (props) {
+    super(props);
 
-  return (
-    <header>
-      <Logo style={{ width, height }} />
-      <ul className="header__list">{ pages.map((page, i) => (
-        <NavItem idx={ i } data={ page } key={ i } />
-      )) }</ul>
-    </header>
-  )
+    this.state = { menuActive: false };
+
+    const isMobile = window.innerWidth <= 1024;
+    const width = isMobile ? '100px' : '120px';
+    const height = '100%';
+
+    this.logoStyle = { width, height };
+  }
+
+  toggleMenu (evt) {
+    evt.currentTarget.classList.toggle('active');
+    this.setState((state) => ({ menuActive: !state.menuActive }))
+  }
+
+  render () {
+    return (
+      <header>
+        <Logo style={ this.logoStyle } />
+        <ul className={ `header__list ${this.state.menuActive ? 'active' : ''}` }>{ pages.map((page, i) => (
+          <NavItem idx={ i } data={ page } key={ i } />
+        )) }</ul>
+        <Hamburger clickfn={ this.toggleMenu.bind(this) } active="0" />
+      </header>
+    ) 
+  }
 }
+
+export default Header;
