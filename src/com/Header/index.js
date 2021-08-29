22c2e32e57572b069883f62f5af844b39eb703be
Lisa Benmore <lbenmore@gmail.com>
2021-08-29 11:32:33 -0700

    moved logo sizing to header css

--- src/com/Header/index.js
+++ src/com/Header/index.js
@@ -9,12 +9,6 @@ class Header extends React.Component {
     super(props);
 
     this.state = { menuActive: false };
-
-    const isMobile = window.innerWidth <= 1024;
-    const width = isMobile ? '100px' : '120px';
-    const height = '100%';
-
-    this.logoStyle = { width, height };
   }
 
   toggleMenu (evt) {
@@ -25,7 +19,7 @@ class Header extends React.Component {
   render () {
     return (
       <header>
-        <Logo style={ this.logoStyle } />
+        <Logo />
         <ul className={ `header__list ${this.state.menuActive ? 'active' : ''}` }>{ pages.map((page, i) => (
           <NavItem idx={ i } data={ page } key={ i } />
         )) }</ul>
